69309902103debcea58613c78f5d56daffd4131d
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-10-16 11:20:58 +0800

    first

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Tailwind/src/output.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="bg-amber-500 text-white">
+        Hello World!
+    </div>
 </body>
 </html>
